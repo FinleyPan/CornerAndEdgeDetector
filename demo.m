fccca6252bd28f0aadbe97b727d5ee00a4f3db23
FinleyPan <cqpanfeng@gmail.com>
2019-01-09 20:11:30 +0800

    add two results

--- demo.m
+++ demo.m
@@ -1,14 +1,12 @@
 samplePathArr = {'data/lena.jpg','data/boldt.jpg','data/building.jpg', ... 
     'data/group.jpg','data/moose.jpg','data/waves.jpg'};
-Img = imread(samplePathArr{6});
+Img = imread(samplePathArr{2});
 
 %% edges detection
-figure(1);
-imshow(uint8(edgeDetector(Img,3,1,1)));
+figure;
+imshow(uint8(edgeDetector(Img,3,1,1)));hold on;
 
 %% corners detection
 [corners,response] = goodCorners(Img,3,0.01,10,true,1/25);
-figure(2);
-imshow(Img);hold on;
-plot(corners(:,1),corners(:,2),'o','Color',[1 0 1]);
+plot(corners(:,1),corners(:,2),'o','Color',[1 0 0]);
 
